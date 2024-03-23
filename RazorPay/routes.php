465b0127aef4906cafc1db982cb2fc738e561893
Sarthak Jain <88922662+Sarthak-07@users.noreply.github.com>
2024-03-23 09:53:23 +0530

    Update routes.php

--- RazorPay/routes.php
+++ RazorPay/routes.php
@@ -7,3 +7,9 @@ Route::post('/razorpay/webhook', [App\Extensions\Gateways\RazorPay\RazorPay::cla
 Route::get('/razorpay/payment/{id}/{order_amount}/{invoiceId}/{key_id}', function ($id, $order_amount, $invoiceId, $key_id) {
     return view('RazorPay::payment', compact('id', 'order_amount', 'invoiceId', 'key_id'));
 })->name('razorpay.payment');
+Route::post('/razorpay/callback/{invoiceId}', function ($invoiceId) {
+    return redirect()->route('clients.invoice.show', $invoiceId);
+})->name('razorpay.callback');
+Route::get('/razorpay/cancel/{invoiceId}', function ($invoiceId) {
+    return redirect()->route('clients.invoice.show', $invoiceId);
+})->name('razorpay.cancel');
